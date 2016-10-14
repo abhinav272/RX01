7dee2bab5968a447259e4109b29d59b3a8e604c3
Abhinav Sharma <abhinav.ih@gmail.com>
2016-10-14 13:14:28 +0530

    Action demonstration

--- src/main/java/JustOperator.java
+++ src/main/java/JustOperator.java
@@ -1,4 +1,6 @@
 import rx.Observable;
+import rx.functions.Action;
+import rx.functions.Action0;
 import rx.functions.Action1;
 import rx.functions.Action2;
 
@@ -9,6 +11,8 @@ public class JustOperator {
 
     private Observable<String> myObservable;
     private Action1<String> customNextAction1;
+    private Action0 customOnCompleteAction;
+    private Action1<Throwable> customErrorAction;
     private Action2<String, String> customNextAction2;
 
     public static void main(String[] args) {
@@ -20,17 +24,37 @@ public class JustOperator {
             @Override
             public void call(String s) {
                 System.out.println(s + " From action1");
+//                System.out.println(10/0);
+
             }
         };
-        justOperator.customNextAction2 = new Action2<String, String>() {
+//        justOperator.customNextAction2 = new Action2<String, String>() {
+//            @Override
+//            public void call(String s, String s2) {
+//                System.out.println(s + "From action2");
+//                System.out.println(s2 + "From action2");
+//            }
+//        };
+
+        justOperator.customErrorAction = new Action1<Throwable>() {
             @Override
-            public void call(String s, String s2) {
-                System.out.println(s + "From action2");
-                System.out.println(s2 + "From action2");
+            public void call(Throwable throwable) {
+                System.out.println("Oops!! error!!");
             }
         };
 
-        justOperator.myObservable.subscribe(justOperator.customNextAction1);
+        justOperator.customOnCompleteAction = new Action0() {
+            @Override
+            public void call() {
+                System.out.println("Process Complete");
+            }
+        };
+
+//        justOperator.myObservable.subscribe(justOperator.customNextAction1);
+        justOperator.myObservable.subscribe(
+                justOperator.customNextAction1,
+                justOperator.customErrorAction,
+                justOperator.customOnCompleteAction);
 
     }
 }
