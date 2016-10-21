094ba22055ec75311677ac42fb6abff8bd6ed476
Abhinav Sharma <abhinav.ih@gmail.com>
2016-10-21 11:35:36 +0530

    Computations Scehduler added

--- src/main/java/SchedulersDemo.java
+++ src/main/java/SchedulersDemo.java
@@ -1,6 +1,8 @@
 import rx.Observable;
+import rx.Scheduler;
 import rx.Subscriber;
 import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by abhinav.sharma on 10/20/2016.
@@ -9,7 +11,9 @@ public class SchedulersDemo {
     public static void main(String[] args) {
         Observable source = Observable.just("Hello", "Animals", "Mama");
         //noinspection unchecked
-        Observable length = source.map(new Func1<String, Integer>() {
+        Observable length = source
+                .subscribeOn(Schedulers.computation())
+                .map(new Func1<String, Integer>() {
             @Override
             public Integer call(String s) {
                 return s.length();
@@ -17,5 +21,16 @@ public class SchedulersDemo {
         });
 
         length.subscribe(o -> System.out.println("Length is : "+o));
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        /*
+        * Since emissions is made from Compution thread subscriber does not
+        * get chance to catch the emissions, Hence sleep is invoked for 2 seconds
+        */
     }
 }
